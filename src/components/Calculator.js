9a37d1f1eed131cf9deb54cc8b938d8088f2b4eb
antonio <ammj24@hotmail.com>
2019-08-25 19:59:40 +0100

    fonts tests refactor

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { calculation, validate } from '../utils/utils'
 
 
 function Elements(props) {
 
     const buttons = [
         ['seven', '7'], ['eight', '8'], ['nine', '9'],  ['percent', '%'], ['clear', 'clear', 'C'],
-        ['four', '4'], ['five', '5'], ['six', '6'], ['multiply', '*'], ['divide', '/'],
+        ['four', '4'], ['five', '5'], ['six', '6'], ['multiply', 'X'], ['divide', '/'],
         ['one', '1'], ['two', '2'], ['three', '3'], ['add', '+'],['subtract', '-'],
         ['zero', '0'], ['decimal', '.'],  ['square-root', '√', '√'], ['equals', '='],
     ]
@@ -33,6 +34,7 @@ export default class Calculator extends React.Component {
         this.number = ''
         this.numberCp = ''
         this.equals = false
+        this.lastExpression = []
 
         this.state = {
             amount: 0
@@ -45,142 +47,167 @@ export default class Calculator extends React.Component {
 
         let input = event.target.value
 
-        if (input === '%') {
-            let num1 = this.fullInput[this.fullInput.length - 2]
-            if (num1 === undefined) {
-                num1 = 1
-            }
-            let percent = num1 * this.number * 0.01   
-            this.setState({amount: percent})
-            this.number = percent
-        } else if (input === '/' | input === '*' | input === '+' | input === '-' ) {
-            console.log('fullinput ', this.fullInput)
-            if ((this.number === '' | this.number === '-') & (!this.equals)) {
-                if(input === '-') {
-                    this.number = this.validate(this.number, input)
-                    this.setState({amount: this.number})
-                } else {
-                    console.log('plus sign and number is empty', this.number)
-                    this.fullInput.splice(-1, 1, input)
-                    this.number = ''
-                    this.setState({amount: this.numberCp})
-                    console.log(this.fullInput)
-                }
-            } else {
-                if (this.number.toString().indexOf('√') !== -1) {
-                    this.number = Math.sqrt(this.number.slice(1))
-                }
-                this.fullInput.push(this.number)
-                let sign = this.fullInput[this.fullInput.length - 2]
+        if (this.state.amount.length >= 10 & (/[0-9|.]+/).test(input) & this.number !== '') {
+            return 
+        }
 
-                if (((input === '+' | input === '-' ) & (this.fullInput.length > 2))
-                    |  (sign === '*' | sign === '/')) {
-                    let result = this.getResult(this.fullInput)
-                    this.setState({amount: result})
-                    this.numberCp = result
-                }
-                this.fullInput.push(input)
-                this.number = ''
+        if (input === '%') {
+            this.percent(input)
+
+        } else if (input === '/' | input === 'X' | input === '+' | input === '-' ) {
+
+            
+            if (this.number === '' & this.state.amount === 0 & input !== '-') {
+                return 0
+            } 
+            // if two or more operators are entered consecutively, the operation performed should be 
+            // the last operator entered
+            else if ((this.number === '' | this.number === '-' | this.number === '√') ){
+                this.consecutiveOpe(input)
+                
+            } else { // if operation follows a normal sequence 
+                this.normalOpe(input)
+
             }
             this.equals = false
         } else if (input === '=') {
-            let result = ''
-            let last = this.fullInput[this.fullInput.length - 2]
-            this.equals = true
+            this.result(input)
 
-            if (this.number === '') {
-                this.number = last
-            }
+        } else if (input === 'clear') {
+            this.clear(input)
+        } else {
+            this.storeNumber(this.number, input)
+        }
+    }
 
-            if (this.number.toString().indexOf('√') !== -1) {
+    storeNumber = (a, b) => {
+        this.number = validate(a, b)
+        this.numberCp = this.number
+        this.setState({amount: this.number})
+    }
+
+    clear = (input) => {
+        input = ''
+        this.number = ''
+        this.numberCp = ''
+        this.lastExpression = []
+        this.fullInput = []
+        this.equals = false
+        this.setState({amount: 0})
+    }
+
+    percent = (input) => {
+        let num1 = this.fullInput[this.fullInput.length - 2]
+        if (this.number === '') {
+            return 0
+        }
+
+        if (num1 === undefined) {
+            num1 = 1
+        }
+        let percent = num1 * this.number * 0.01   
+        this.setState({amount: percent})
+        this.number = percent
+    }
+
+    result = (input) => {
+        let result = ''
+        let lastOne = this.fullInput[this.fullInput.length - 1]
+        let lastTwo = this.fullInput[this.fullInput.length - 2]
+        this.equals = true
+        
+        if (this.state.amount === 0) {
+            return 0
+        } else if (typeof lastOne === 'number') {
+            this.fullInput.splice(-1, 1, this.lastExpression[0])
+            this.number = this.lastExpression[1]
+        } else if (this.number === '') {
+            this.number = lastTwo
+        } else if (this.number.toString().indexOf('√') !== -1) {
+            if (this.number[0] === '-') {
+                this.number = -Math.sqrt(this.number.slice(2))
+            } else {
                 this.number = Math.sqrt(this.number.slice(1))
             }
+        }
 
+        if (this.number !== '') {
+            // add number to array
             this.fullInput.push(this.number)
-
-            if (this.number !== '') {
-                result = this.getResult(this.fullInput)
-
-
-                this.fullInput.push(result)
-
-
-
-
-                this.setState({amount: result})
-                this.numberCp = result
-                input = ''
-                this.number = ''
-                console.log('after equals ', this.fullInput)
-            } 
-        } else if (input === 'clear') {
+            this.lastExpression.push(this.number)
+        }
+        if (this.number !== '') {
+            result = this.getResult(this.fullInput)
+            this.fullInput.push(result)
+            this.setState({amount: result})
+            this.numberCp = result
             input = ''
             this.number = ''
-            this.numberCp = ''
-            this.fullInput = []
-            this.setState({amount: 0})
-        } else {   
-            this.number = this.validate(this.number, input)
-            this.numberCp = this.number
+        } 
+    }
+
+    consecutiveOpe = (input) => {
+        if (input === '-' & !this.equals) { // - operator
+            this.number = validate(this.number, input)
             this.setState({amount: this.number})
+        } else {  // + or * or / operator
+            this.fullInput.splice(-1, 1, input)
+            this.number = ''
+            this.setState({amount: this.numberCp})
         }
     }
 
-    validate = (a, c) => {
-        let str= ''
-
-        if (a === '0' && c === '0') {
-            return '0'
-        } else if (a === '' && c === '.') {
-            return '0.'
-        } else if (c === '.' && a.toString().indexOf('.') !== -1) {
-            str = a
-        } else if (c === '-' && a.toString().indexOf('-') !== -1) { 
-            str = a
-        } else {
-            str = a + c
+    normalOpe = (input) => {
+        if (this.number.toString().indexOf('√') !== -1) {
+            if (this.number[0] === '-') {
+                this.number = -Math.sqrt(this.number.slice(2))
+            } else {
+                this.number = Math.sqrt(this.number.slice(1))
+            }
         }
 
-        return str  
-    }
-
-    calculation = (num1, ope, num2) => {
-                
-        num1 = parseFloat(num1)
-        num2 = parseFloat(num2)
-
-        switch(ope) {
-            case '/':
-                return num1 / num2
-            case '*':
-                return num1 * num2
-            case '+':
-                return num1 + num2
-            case '-':
-                return num1 - num2
-            case '√':
-                return Math.sqrt(num1)
-            default:
-                return ''
+        if (this.number !== '') {
+            // add number to array
+            this.fullInput.push(this.number)
         }
+
+        let sign = this.fullInput[this.fullInput.length - 2]
+        if (((input === '+' | input === '-' ) & (this.fullInput.length > 2))
+            |  (sign === 'X' | sign === '/')) {
+            let result = this.getResult(this.fullInput)
+            this.setState({amount: result})
+            this.numberCp = result
+        }
+
+        this.lastExpression.push(input)
+        // add operator to array and set number to empty value
+        this.fullInput.push(input)
+        this.number = ''
     }
 
     getResult = (input) => {
-        const symbols = [['/', '*'], ['+', '-']]
+        const symbols = [['/', 'X'], ['+', '-']]
         const inputCp = input.slice(0)
 
         for(let i=0; i<symbols.length; i++) {
             inputCp.forEach((e) => {
                 if (symbols[i][0] === e | symbols[i][1] === e) {
                     let idx = input.indexOf(e)
-                    let res = this.calculation(input[idx-1], e, input[idx+1])
+                    let res = calculation(input[idx-1], e, input[idx+1])
                     input.splice(idx-1, 3, res)
                 }
             })
         }
         let result = input[0]
 
-        return isNaN(result)?'Error':result.toString().length>10?result.toString().slice(0,10):result
+        if (isNaN(result)) {
+            return 'Error'
+        } else if (result.toString().length > 10) {
+            return result.toString().slice(0,11)
+        } else {
+            return result
+        }
+
     }
 
     render() {
@@ -194,3 +221,6 @@ export default class Calculator extends React.Component {
         )
     }
 }
+
+
+
---
+++ src/utils/utils.js
@@ -0,0 +1,45 @@
+
+export function calculation(num1, ope, num2) {
+                
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+
+    switch(ope) {
+        case '/':
+            return num1 / num2
+        case 'X':
+            return num1 * num2
+        case '+':
+            return num1 + num2
+        case '-':
+            return num1 - num2
+        case '√':
+            return Math.sqrt(num1)
+        default:
+            return ''
+    }
+}
+
+export function validate(a, c) {
+    let str = ''
+
+    if (a === '0' && c === '0') {
+        return '0'
+    } else if (a === '0' && c !== '.') {
+        return c
+    } else if (a === '' && c === '.') {
+        return '0.'
+    } else if (a === '-' && c === '.') {
+        return '-0.'
+    } else if (c === '.' && a.toString().indexOf('.') !== -1) {
+        str = a
+    } else if (c === '-' && a.toString().indexOf('-') !== -1) { 
+        str = a
+    } else if (c === '√' && a.toString().indexOf('√') !== -1) { 
+        str = a
+    } else {
+        str = a + c
+    }
+
+    return str  
+}
